552d6b483f26aa8af7bc8fe4b0654e8650070e5d
CyrilBaah <cyril.baah@amalitech.org>
2021-08-16 12:12:48 +0000

    Refactor - declineRequest

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// var methodOverride = require('method-override')
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
@@ -12,7 +11,6 @@ require('dotenv').config();
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(methodOverride('_method'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
--- public/css/main.js
+++
@@ -1,3 +0,0 @@
-const ne = () => {
-    alert('works')
-}
