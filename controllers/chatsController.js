1061c0deb5a5c28cd56d5307875f44ab0952fe84
CyrilBaah <cyril.baah@amalitech.org>
2021-08-17 15:37:15 +0000

    Feature - Chats

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const { User } = require('../models');
+
 
 exports.getIndex = async (req, res) => {
-
-    res.render('chats/index')
+    const authToken = await req.cookies['jwt'];
+    const users = await User.findAll();
+    users.forEach(user => {
+        if(user.token === authToken){
+            res.render('chats/index', { 
+                title: 'ChatApp | Chat Page',
+                path: '/chats',
+                isAuthenticated: authToken,
+                users: users,
+                username: user.username
+            });
+        }
+    }); 
 }
+
+
+
+
 
 
 
--- routes/chats.js
+++ routes/chats.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const chatsController = require('../controllers/chatsController');
+const auth = require('../middleware/auth');
 
 
-router.get('/chats', chatsController.getIndex);
+router.get('/chats',auth, chatsController.getIndex);
 
 
 module.exports = router;
